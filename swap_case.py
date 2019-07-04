fcf6e05e811504a6eab7277fbcf89d3315666015
Iryna Yefanova <yefanovairyna@gmail.com>
2019-07-05 00:28:17 +0200

    swap_case added

---
+++ swap_case.py
@@ -0,0 +1,12 @@
+def swap_case(s):
+    s=list(s)
+    for i, c in enumerate(s):
+        if c.isupper()==True:
+           s[i] = c.lower()
+            
+        else:
+            s[i]=c.upper()
+            
+    return "".join(s)
+
+if __name__ == '__main__':
